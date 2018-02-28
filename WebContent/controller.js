5e0a93962af42ea0bc922794e19c7e20b8e5edad
Sergio <Sergio@DESKTOP-MVPRIN3>
2018-02-28 22:29:32 +0100

    sasensio: Cofre Abierto

--- WebContent/controller.js
+++ WebContent/controller.js
@@ -1,9 +1,26 @@
 var app= angular.module("despedidApp",[]);
 
 app.controller("despedidaController",["$scope",function(despedida){
-	despedida.message='HOLA';
+	
+//Function to show/hide User Login Table
+despedida.showTableData=function(){		
+	if(!despedida.showTable){
+		despedida.showTable=true;
+	}
+	else{
+		despedida.showTable=false;
+	}	
+	window.scrollTo(0,document.body.scrollHeight);
+}
+
+//Function that checks user and pass
+despedida.checkData=function(){	
+	despedida.errorMessage='';
+	if(despedida.user=='Lacito' && despedida.pass=='chocolate') 
+		despedida.cofreAbierto=true;
+	else{
+		despedida.errorMessage='Tiooooo que te lo he enviado por correo!!!!';
+	}
+}	
 }]);
 
-function showLogin(){
-	document.getElementById("cofreClaves").visibility = "hidden";
-} 
